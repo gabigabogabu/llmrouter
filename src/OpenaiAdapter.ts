219a243769ac27ec479d1284c0f6fa52b681b0ad
gabigabogabu <gaschu95@gmail.com>
2025-06-02 14:11:54 +0200

    scope special patches to host

--- src/OpenaiAdapter.ts
+++ src/OpenaiAdapter.ts
@@ -4,6 +4,7 @@ import type { LlmHostAdapter } from "./LlmHostAdapter";
 
 export class OpenaiAdapter implements LlmHostAdapter {
   private client: OpenAI;
+  private host: string;
 
   private _modelBlocklist: string[] = [
     'codex-mini-latest', // does not support chat completions
@@ -13,27 +14,32 @@ export class OpenaiAdapter implements LlmHostAdapter {
 
   constructor(params: ClientOptions) {
     this.client = new OpenAI(params);
+    this.host = new URL(this.client.baseURL).host;
   }
 
   async listModels(): Promise<OpenAI.Models.Model[]> {
     const models = [];
     for await (const model of this.client.models.list()) {
-      if (!this._modelBlocklist.includes(model.id)) 
+      if (!this._modelBlocklist.includes(model.id))
         models.push(model);
     }
     return models;
   }
 
   async chat(...params: Parameters<typeof OpenAI.prototype.chat.completions.create>): Promise<ReturnType<typeof OpenAI.prototype.chat.completions.create>> {
-    if (params[0].model === 'gpt-4o-mini-search-preview') {
-      // Model incompatible request argument supplied: presence_penalty
-      params[0].presence_penalty = undefined;
-    }
-    if (params[0].model === 'o1-mini') {
-      // Unsupported value: 'messages[0].role' does not support 'system' with this model.
-      params[0].messages = params[0].messages.map(message => message.role === 'system' ? { ...message, role: 'user' } : message);
-      // Unsupported value: 'temperature' does not support 2 with this model. Only the default (1) value is supported.
-      params[0].temperature && (params[0].temperature = 1);
+    if (this._modelBlocklist.includes(params[0].model)) 
+      throw new Error(`Model ${params[0].model} is not supported`);
+    if (this.host === 'api.openai.com') {
+      if (params[0].model === 'gpt-4o-mini-search-preview') {
+        // Model incompatible request argument supplied: presence_penalty
+        params[0].presence_penalty = undefined;
+      }
+      if (params[0].model === 'o1-mini') {
+        // Unsupported value: 'messages[0].role' does not support 'system' with this model.
+        params[0].messages = params[0].messages.map(message => message.role === 'system' ? { ...message, role: 'user' } : message);
+        // Unsupported value: 'temperature' does not support 2 with this model. Only the default (1) value is supported.
+        params[0].temperature && (params[0].temperature = 1);
+      }
     }
     return await this.client.chat.completions.create(...params);
   }
